0eee58fd4b97de266e2c61c004dc58d8697329e6
Juan Cobian <cobianjuan19@gmail.com>
2020-04-16 16:56:15 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -17,6 +17,7 @@ def age_appropriate_birthday(birthday_kids)
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
       puts "You are too old to be here!"
+    end
   end
 end
 
